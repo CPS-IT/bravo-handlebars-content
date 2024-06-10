36802c1402c5a8d73b20ad722d1b52daa010fe63
Dirk Wenzel <wenzel@cps-it.de>
2024-06-10 08:37:54 +0200

    format file size in human readable units

--- Classes/Service/FileLinkService.php
+++ Classes/Service/FileLinkService.php
@@ -17,7 +17,7 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 final class FileLinkService
 {
-    const FILE_PROPERTIES = [
+    public const FILE_PROPERTIES = [
         'title',
         'description',
         'download_name',
@@ -28,6 +28,11 @@ final class FileLinkService
         'url',
         'accessible',
     ];
+
+    public const FILE_SIZE_UNITS = [
+        'B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'
+    ];
+
 
     /**
      * @param mixed $fileReference
@@ -43,12 +48,33 @@ final class FileLinkService
 
         foreach ($properties as $property) {
             if ($fileReference->hasProperty($property)) {
-                $downloadItem[GeneralUtility::underscoredToLowerCamelCase($property)] =
-                    $fileReference->getProperty($property);
+                $value = $fileReference->getProperty($property);
+
+                if ($property === 'size') {
+                    $value = self::formatFileSize($value);
+                }
+
+                $downloadItem[GeneralUtility::underscoredToLowerCamelCase($property)] = $value;
             }
         }
 
         return $downloadItem;
     }
 
+    /**
+     * Format a file size in bytes in suitable units.
+     * Note: this function might return invalid values for file sizes over 2GB!
+     * see https://www.php.net/manual/en/function.filesize.php (Note on Return Values).
+     * @param $size
+     * @return string
+     */
+    public static function formatFileSize(int $size): string
+    {
+        $power = $size > 0 ? floor(log($size, 1024)) : 0;
+        return sprintf("%s %s", number_format(
+            $size / (1024 ** $power),
+            2,
+            '.', // note: we should use the appropriate thousands separator for the curren language
+            ','), self::FILE_SIZE_UNITS[$power]);
+    }
 }
